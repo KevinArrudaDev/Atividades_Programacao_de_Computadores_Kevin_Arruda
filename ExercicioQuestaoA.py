499ee30a51e3a815244aff645bbeadb50d99aaf8
KevinArrudaDev <kevin.arruda@proton.me>
2026-03-29 12:03:09 -0300

    feature: criação da questão A

---
+++ ExercicioQuestaoA.py
@@ -0,0 +1,10 @@
+"""
+Elabore um algoritmo que receba dois números, calcule a multiplicação entre eles e apresente o
+resultado.
+"""
+primeiroNumero = float(input("Digite o primeiro número: "))
+segundoNumero = float(input("Digite o segundo número: "))
+
+calculoEntreNumeros = primeiroNumero * segundoNumero
+
+print("A resultado da multiplicação entre os números é: ", calculoEntreNumeros)
