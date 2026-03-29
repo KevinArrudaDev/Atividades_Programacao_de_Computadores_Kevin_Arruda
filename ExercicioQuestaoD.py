10296f1cc42e84bb97167b906c010102ff34f4fb
KevinArrudaDev <kevin.arruda@proton.me>
2026-03-29 12:19:59 -0300

    feature: criação da questão D

---
+++ ExercicioQuestaoD.py
@@ -0,0 +1,10 @@
+"""
+Elabore um algoritmo que receba dois números e apresente o resultado da subtração entre eles.
+"""
+
+primeiroValor = float(input("Digite o primeiro valor: "))
+segundoValor = float(input("Digite o segundo valor: "))
+
+subtracaoEntreNumeros = primeiroValor - segundoValor
+
+print("A subtração entre os números corresponde a: ", subtracaoEntreNumeros)
